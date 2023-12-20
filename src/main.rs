b1ab2b30d8790770bd53a43e76cc35324d94f3fb
Tomasz Bawor <bawortomasz@gmail.com>
2023-12-20 22:35:44 +0100

    Add char movement

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,22 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(ImagePlugin::default_nearest())
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Comston".into(),
+                        resolution: (640.0, 480.0).into(),
+                        resizable: false,
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .build(),
+        )
         .add_systems(Startup, setup)
-        .add_plugins((DefaultPlugins))
+        .add_systems(Update, char_movement)
         .run()
 }
 
@@ -20,4 +34,25 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         texture,
         ..default()
     });
+}
+
+fn char_movement(
+    mut characters: Query<(&mut Transform, &Sprite)>,
+    input: Res<Input<KeyCode>>,
+    time: Res<Time>,
+) {
+    for (mut transf, _) in &mut characters {
+        if input.pressed(KeyCode::W) {
+            transf.translation.y += 100.0 * time.delta_seconds();
+        }
+        if input.pressed(KeyCode::S) {
+            transf.translation.y -= 100.0 * time.delta_seconds();
+        }
+        if input.pressed(KeyCode::A) {
+            transf.translation.x -= 100.0 * time.delta_seconds();
+        }
+        if input.pressed(KeyCode::D) {
+            transf.translation.x += 100.0 * time.delta_seconds();
+        }
+    }
 }
